b84917fbfa69b59083b88a50292ac4495590db70
AshHasib <ashhasib54@gmail.com>
2019-11-07 12:12:40 +0600

    Nested URL structure implemented

--- polls/urls.py
+++ polls/urls.py
@@ -6,9 +6,19 @@
 from django.urls import path
 from . import views
 
+'''
+urlpatterns = [
+    path('polls/', views.PollList.as_view(), name = 'polls'),
+    path('polls/<int:pk>/', views.PollDetail.as_view(), name = 'poll_detail'),
+    path('choices/', views.ChoiceList.as_view(), name = 'choice_list'),
+    path('vote/', views.CreateVote.as_view(), name = 'create_vote'),
+]
+
+'''
+
 urlpatterns = [
     path('polls/', views.PollList.as_view(), name = 'polls'),
     path('polls/<int:pk>', views.PollDetail.as_view(), name = 'poll_detail'),
-    path('choices/', views.ChoiceList.as_view(), name = 'choice_list'),
-    path('vote/', views.CreateVote.as_view(), name = 'create_vote'),
+    path('polls/<int:pk>/choices/', views.ChoiceList.as_view(), name = 'choice_list'),
+    path('polls/<int:pk>/choices/<int:choice_pk>/vote/', views.CreateVote.as_view(), name = 'create_vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -10,7 +10,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
-
+from rest_framework import response, status
 
 
 # Create your views here.
@@ -44,6 +44,8 @@ API using generic views
 
 '''
 
+
+
 class PollList(generics.ListCreateAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
@@ -53,7 +55,7 @@ class PollDetail(generics.RetrieveDestroyAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
-
+'''
 class ChoiceList(generics.ListCreateAPIView):
     queryset = Choice.objects.all()
     serializer_class = ChoiceSerializer
@@ -61,6 +63,36 @@ class ChoiceList(generics.ListCreateAPIView):
 
 class CreateVote(generics.CreateAPIView):
     serializer_class = VoteSerializer
+
+'''
+
+# Better URL structuring by changing the views
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id = self.kwargs['pk'])
+        return queryset
+
+    serializer_class = ChoiceSerializer
+
+class CreateVote(APIView):
+    serializer_class = VoteSerializer
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get('voted_by')
+        data = {'choice': choice_pk,'poll':pk, 'voted_by': voted_by}
+
+        serializer = VoteSerializer(data = data)
+
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
 
 
 
